8f0653471d8f0b111acc42a001380d4526bccee1
Patrick Colby <pcolby0@gmail.com>
2014-08-31 14:12:35 -0700

    Added hw1q4 folder

---
+++ hw1/hw1q4/hw1q4/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  hw1q4
+//
+//  Created by Patrick Colby on 8/31/14.
+//  Copyright (c) 2014 Patrick Colby. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
